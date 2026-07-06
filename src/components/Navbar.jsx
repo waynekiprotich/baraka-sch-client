3e25bb2f59181c7d4824e428ab6fd9252d6cf9a5
[dyad] <waynekip123@icloud.com>
2026-07-06 23:57:35 +0300

    [dyad] Imported useLocation in Navbar to fix undefined error - wrote 1 file(s)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState, useRef } from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useLocation } from "react-router-dom";
 import useTheme from "../hooks/useTheme.js";
 import "./../styles/navbar.css";
 
