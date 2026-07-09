051adeba335076d2ddab27cbbde9a98f5ef4c324
[dyad] <waynekip123@icloud.com>
2026-07-09 12:37:06 +0300

    [dyad] wrote 2 file(s) + extra files edited outside of Dyad

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 export default defineConfig({
-  plugins: [dyadComponentTagger(), react()],
+  plugins: [react()],
 });
